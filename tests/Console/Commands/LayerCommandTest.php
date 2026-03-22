c8ba5afad2eaed5cf176c9be20168750b63ba9c8
Gautier DELEGLISE <gautier.deleglise@gmail.com>
2026-03-22 21:26:46 +0100

    Add test for creating layer with no generators
    Add testItCreatesLayerWithNoGenerators to LayerCommandTest. The new test runs the osdd:layer artisan command with no generators selected and composer update skipped, asserts a successful exit, verifies composer.json is created, and confirms that generator artifacts (migrations, models, factories, seeders, providers, feature tests, controllers, policies) are not created.

--- tests/Console/Commands/LayerCommandTest.php
+++ tests/Console/Commands/LayerCommandTest.php
@@ -131,6 +131,26 @@ class LayerCommandTest extends TestCase
         $this->assertFilenameNotExists('functional/my-layer/src/Policies');
     }
 
+    public function testItCreatesLayerWithNoGenerators(): void
+    {
+        $this->artisan('osdd:layer')
+            ->expectsQuestion('Layer name', 'my-layer')
+            ->expectsChoice('Which generators should be run?', [], $this->allGenerators())
+            ->expectsConfirmation('Run composer update now?', 'no')
+            ->assertExitCode(0);
+
+        $this->assertFilenameExists('functional/my-layer/composer.json');
+
+        $this->assertFilenameNotExists('functional/my-layer/database/migrations');
+        $this->assertFilenameNotExists('functional/my-layer/src/Models');
+        $this->assertFilenameNotExists('functional/my-layer/database/factories');
+        $this->assertFilenameNotExists('functional/my-layer/database/seeders');
+        $this->assertFilenameNotExists('functional/my-layer/src/Providers');
+        $this->assertFilenameNotExists('functional/my-layer/tests/Feature');
+        $this->assertFilenameNotExists('functional/my-layer/src/Http/Controllers');
+        $this->assertFilenameNotExists('functional/my-layer/src/Policies');
+    }
+
     public function testItDoesNotRegisterServiceProviderInLayerComposerJsonWhenNotSelected(): void
     {
         $this->artisan('osdd:layer')
